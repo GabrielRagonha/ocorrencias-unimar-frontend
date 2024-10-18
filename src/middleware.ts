1fc006b192e9b362512478c9e79660a03d7a81a5
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2024-10-17 23:03:27 -0300

    feat: create new middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: ["/:path*"],
+  matcher: "/((?!api|static|.*\\..*|_next).*)",
 };
 
 export function middleware(request: NextRequest) {
-  // if (request.nextUrl.pathname !== "/") {
-  //   return NextResponse.redirect(new URL("/", request.url));
-  // }
+  const token = request.cookies.get("ocorrencias_token");
+  const pathname = request.nextUrl.pathname;
 
+  // Se o token estiver presente e a rota for '/' ou '/criar-conta', redireciona para '/ocorrencias'
+  if (token) {
+    if (pathname === "/" || pathname === "/criar-conta") {
+      return NextResponse.redirect(new URL("/ocorrencias", request.url));
+    }
+  }
+
+  // Caso não haja token, o usuário não poderá acessar nenhuma página além do login e cadastro
+  if (!token) {
+    if (pathname !== "/" && pathname !== "/criar-conta") {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  // Se não houver necessidade de redirecionamento, continua para a próxima rota
   return NextResponse.next();
 }
